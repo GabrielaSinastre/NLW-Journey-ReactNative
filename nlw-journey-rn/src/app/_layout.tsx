b662274490dbf10d1c3b4f66d1a2b5b05f8ce90b
Gabriela Sinastre <55140599+GabrielaSinastre@users.noreply.github.com>
2024-07-14 00:58:48 -0300

    feat: trip integration with api

--- nlw-journey-rn/src/app/_layout.tsx
+++ nlw-journey-rn/src/app/_layout.tsx
@@ -1,7 +1,37 @@
-import { Slot } from 'expo-router'
-import '@/styles/global.css'
+import "@/styles/global.css"
+import "@/utils/dayjsLocaleConfig"
 
-// Slot carrega as informações do meu global.css e repassa em toda a aplicação
+import { View, StatusBar } from "react-native"
+import { Slot } from "expo-router"
+
+import {
+  useFonts,
+  Inter_500Medium,
+  Inter_400Regular,
+  Inter_600SemiBold,
+} from "@expo-google-fonts/inter"
+
+import { Loading } from "@/components/loading"
+
 export default function Layout() {
-  return <Slot />
+  const [fontsLoaded] = useFonts({
+    Inter_500Medium,
+    Inter_400Regular,
+    Inter_600SemiBold,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
+  return (
+    <View className="flex-1 bg-zinc-950">
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Slot />
+    </View>
+  )
 }
